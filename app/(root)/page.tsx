deab73bab6babf822c78804c90c83947d1ca96b4
Muzaffer <thepitohui@gmail.com>
2023-08-02 15:55:58 +0300

    feat:created modal components, implemented modal to the root page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,28 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+"use client"
+
+import { useEffect } from "react";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { Modal } from "@/components/ui/modal";
+
+
 
 const SetupPage = () => {
+  const onOpen = useStoreModal((state)=>state.onOpen);
+  const isOpen = useStoreModal((state)=>state.isOpen);
+
+  useEffect(()=>{
+    if(!isOpen){
+      onOpen();
+    }
+  },[isOpen,onOpen]);
+
   return (
     <div className="p-4">
-      <UserButton afterSignOutUrl="/" />
+      Root page
     </div>
   );
 }
-
+ 
 export default SetupPage;
   
