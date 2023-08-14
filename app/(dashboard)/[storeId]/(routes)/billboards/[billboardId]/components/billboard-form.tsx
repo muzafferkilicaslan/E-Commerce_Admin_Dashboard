b8cb4ec8b6e6b47390246cda29b6e064c2758ac0
Muzaffer <thepitohui@gmail.com>
2023-08-15 00:09:14 +0300

    feat: implemented data-table on billboard page, created table and actions, fix small api issue on middleware.ts

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -163,7 +163,6 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
                     </Button>
                 </form>
             </Form>
-            <Separator />
         </>
     )
 }
--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -5,9 +5,20 @@ import { useParams, useRouter } from "next/navigation"
 import { Button } from '@/components/ui/button'
 import { Heading } from '@/components/ui/heading'
 import { Separator } from '@/components/ui/separator'
+import { BillboardColumn, columns } from "./columns"
+import { DataTable } from "@/components/ui/data-table"
+import { ApiList } from "@/components/ui/api-list"
 
 
-const BillboardClient = () => {
+
+interface BillboardClientProps{
+  data: BillboardColumn[]
+}
+
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({
+  data
+}) => {
   const router = useRouter();
   const params = useParams();
 
@@ -15,7 +26,7 @@ const BillboardClient = () => {
     <>
     <div className="flex items-center justify-between">
         <Heading 
-            title="Billboards (0)"
+            title={`Billboards (${data.length})`}
             description='Manage billboards for your store'
         />
         <Button onClick={()=>router.push(`/${params.storeId}/billboards/new`)}>
@@ -24,8 +35,10 @@ const BillboardClient = () => {
         </Button>
     </div>
     <Separator />
+    <DataTable searchKey="label" columns={columns} data={data} />
+    <Heading title="API" description="API calls for Billboards" />
+    <Separator />
+    <ApiList entityName="billboards" entityIdName="billboardId" />
     </>
   )
 }
-
-export default BillboardClient
