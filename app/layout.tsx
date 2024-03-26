1041fbd0cd0f05b34c8d0cd165d68afb87b834a3
Muzaffer Kılıçaslan <thepitohui@gmail.com>
2024-03-26 04:24:54 +0300

    feat: added dark mode to admin

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,34 @@
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ModalProvider } from "@/providers/modal-provider";
+import { ToasterProvider } from "@/providers/toast-provider";
+import { ThemeProvider } from "@/providers/theme-provieder";
 
-import { Inter } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
-import { ModalProvider } from '@/providers/modal-provider'
-import { ToasterProvider } from '@/providers/toast-provider'
+import "./globals.css";
 
-import './globals.css'
-
-
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Admin Dashboard',
-  description: 'Admin Dashboard',
-}
+  title: "Admin Dashboard",
+  description: "Admin Dashboard",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-        <ToasterProvider />
-        <ModalProvider />
-          {children}
-          </body>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <ToasterProvider />
+            <ModalProvider />
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
